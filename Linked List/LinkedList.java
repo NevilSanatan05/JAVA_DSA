2af38d081861082c8cff532fc2b1a9552895a4a8
nevil811 <nevilbarick69@gmail.com>
2026-08-27 12:58:01 +0530

    Linked List - Remove first amd last

--- Linked List/LinkedList.java
+++ Linked List/LinkedList.java
@@ -77,15 +77,59 @@ public class LinkedList {
     temp.next = newNode;
   }
 
+  public int removeFirst() {
+    if (size == 0) {
+      System.out.println("LL is empty");
+      return Integer.MIN_VALUE;
+    } else if (size == 1) {
+      int val = head.data;
+      head = tail = null;
+      size = 0;
+      return val;
+    }
+    int val = head.data;
+    head = head.next;
+    size--;
+    return val;
+  }
+
+  public int removeLast() {
+    if (size == 0) {
+      System.out.println("LL is empty");
+      return Integer.MIN_VALUE;
+    } else if (size == 1) {
+      int val = head.data;
+      head = tail = null;
+      size = 0;
+      return val;
+    }
+    // prev : i = size-2
+    Node prev = head;
+    for (int i = 0; i < size - 2; i++) {
+      prev = prev.next;
+    }
+    int val = prev.next.data; // tail.data
+    prev.next = null;
+    tail = prev;
+    size--;
+    return val;
+
+  }
+
   public static void main(String args[]) {
     LinkedList ll = new LinkedList();
     ll.addFirst(2);
     ll.addFirst(1);
-    ll.addLast(3);
     ll.addLast(4);
-    ll.add(2, 9);
+    ll.addLast(5);
+    ll.add(2, 3);
     ll.print();
-   System.out.println("Size of linked list: " + size);
+    System.out.println("Size of linked list: " + size);
+    ll.removeFirst();
+    ll.print();
 
+    ll.removeLast();
+    ll.print();
+    System.out.println(ll.size);
   }
 }
