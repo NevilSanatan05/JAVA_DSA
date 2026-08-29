fe538202ddb05fa71a533d78c8ad61fe43c2059b
nevil811 <nevilbarick69@gmail.com>
2026-08-29 14:44:43 +0530

    Linked List - Check Palindrome

--- Linked List/LinkedList.java
+++ Linked List/LinkedList.java
@@ -190,6 +190,51 @@ public class LinkedList {
     return;
   }
 
+  // Slow-Fast Approach
+  public Node findMid(Node head) {
+    Node slow = head;
+    Node fast = head;
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next; // +1
+      fast = fast.next.next; // +2
+    }
+    return slow; // slow is my mid node
+  }
+
+  public boolean checkPalindrome() {
+    if (head == null || head.next == null) {
+      return true;
+    }
+    // step1 - find mid
+    Node midNode = findMid(head);
+
+    // step2 - reverse 2nd half
+    Node prev = null;
+    Node curr = midNode;
+    Node next;
+    while (curr != null) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+
+    Node right = prev; // right half head
+    Node left = head;
+
+    // step3 - check if 1st half and 2nd half are equal
+
+    while (right != null) {
+      if (left.data != right.data) {
+        return false;
+      }
+      left = left.next;
+      right = right.next;
+    }
+    return true;
+  }
+
   public static void main(String args[]) {
     LinkedList ll = new LinkedList();
     ll.addFirst(2);
@@ -201,7 +246,9 @@ public class LinkedList {
 
     // ll.reverse();
     // ll.print(); // 5->4->3->2->1->null
-    ll.deleteNthFromEnd(3);
-    ll.print();
+    // ll.deleteNthFromEnd(3);
+    // ll.print();
+
+    System.out.println(ll.checkPalindrome());
   }
 }
