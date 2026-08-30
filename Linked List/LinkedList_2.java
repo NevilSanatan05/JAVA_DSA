886ad2a85aabba92b0d4524f0fd778fe5caf4a7a
nevil811 <nevilbarick69@gmail.com>
2026-08-30 15:10:51 +0530

    Linked List - Remove Cycle in LL

--- Linked List/LinkedList_2.java
+++ Linked List/LinkedList_2.java
@@ -12,7 +12,7 @@ public class LinkedList_2 {
 
   static Node head;
 
-  public static boolean isCycle() { //Floyds 
+  public static boolean isCycle() { // Floyds
     Node slow = head;
     Node fast = head;
 
@@ -26,11 +26,46 @@ public class LinkedList_2 {
     return false;
   }
 
+  public static void removeCycle() {
+    //Detect cycle
+    Node slow = head;
+    Node fast = head;
+    boolean cycle = false;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (fast == slow) {
+        cycle = true;
+        break;
+      }
+    }
+    if (cycle == false) {
+      return;
+    }
+
+    //Find meeting point
+    slow = head;
+    Node prev = null;
+    while (slow != fast) {
+      prev = fast;
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    // remove cycle -> last.next=null
+    prev.next = null;
+
+  }
+
   public static void main(String[] args) {
     head = new Node(1);
-    head.next = new Node(2);
+    Node temp = new Node(2);
+    head.next=temp;
     head.next.next = new Node(3);
-    head.next.next.next = head;
+    head.next.next.next = temp;
+
+    System.out.println(isCycle());
+    removeCycle();
     System.out.println(isCycle());
 
   }
