fb1cc9f1db11e30d938f9f6b012b4e5c596b736f
nevil811 <nevilbarick69@gmail.com>
2026-09-04 13:48:50 +0530

    Basic java

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,9 +1,91 @@
-public class Basic{
-  public static void main(String args[]){
-    System.out.println("*****");
-    System.out.println("   * ");
-    System.out.println("  *  ");
-    System.out.println(" *   ");
-    System.out.println("*****");
-  }
-}
+// public class Basic{
+// public static void main(String args[]){
+// System.out.println("*****");
+// System.out.println(" * ");
+// System.out.println(" * ");
+// System.out.println(" * ");
+// System.out.println("*****");
+// }
+// }
+
+
+// (*/) > (+-)
+
+
+// Conditional Statements
+// public class Basic{
+// public static void main(String args[]){
+// int marks = 45;
+// if(marks>90){
+// System.out.println("Excellent");
+// }
+// else if (marks>80 && marks<=90){
+// System.out.println("good");
+// }
+// else if (marks>70 && marks<=80){
+// System.out.println("fair");
+// }
+// else if (marks>60 && marks<=70){
+// System.out.println("meets expectations");
+// }
+// else{
+// System.out.println("below par");
+// }
+// }
+// }
+
+
+// Loops
+// While Loop
+// public class Basic{
+// public static void main(String args[]){
+// int i =0;
+// while(i<=10){
+// System.out.println(i);
+// i++;
+// }
+// }
+// }
+
+// For Loop
+// public class Basic{
+// public static void main(String args[]){
+// for(int i=1;i<=10;i++){
+// System.out.println(i);
+// }
+// }
+// }
+
+// Do While Loop
+// public class Basic{
+// public static void main(String args[]){
+// int i =1;
+// do{
+// System.out.println(i);
+// i++;
+// }
+// while(i<=10);
+// }
+// }
+
+
+// import java.util.*;
+// public class Basic{
+//   public static void main(String args[]){
+   
+//    Scanner sc = new Scanner(System.in);
+//    System.out.print("Enter your age : ");
+//    int i = sc.nextInt();
+//    if(i>=18){
+    
+//      System.out.println("Eligible to vote");
+//    }
+//    else{
+//      System.out.println("Not Eligible to vote");
+//    } 
+//    System.out.print("Enter your name : ");
+//    String name = sc.next();
+//    System.out.println("Hello " + name);
+
+//   }
+// }
