6980cbb0d6edeb2cd192661fdc841f66a4114891
nevil811 <nevilbarick69@gmail.com>
2026-09-08 12:59:27 +0530

    Basics java

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -89,3 +89,17 @@
 
 //   }
 // }
+
+
+//isPrime
+
+import java.util.*;
+public class Basic {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number : ");
+    int n = sc.nextInt();
+    
+  }
+}
