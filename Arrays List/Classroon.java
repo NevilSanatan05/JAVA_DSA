b6c1d35671feea4882ad539d9362be0015f50662
nevil811 <nevilbarick69@gmail.com>
2026-09-10 13:03:59 +0530

    ArrayList

--- Arrays List/Classroon.java
+++ Arrays List/Classroon.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
+//import java.util.Collections;
 
 public class Classroon {
+  // public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
+  // int temp = list.get(idx1);
+  // list.set(idx1, list.get(idx2));
+  // list.set(idx2, temp);
+  // }
+
   public static void main(String[] args) {
 
     ArrayList<Integer> list = new ArrayList<>();
-    ArrayList<String> list2 = new ArrayList<>();
-    ArrayList<Boolean> list3 = new ArrayList<>();
+    // ArrayList<String> list2 = new ArrayList<>();
+    // ArrayList<Boolean> list3 = new ArrayList<>();
 
     list.add(2);
     list.add(5);
@@ -13,16 +20,31 @@ public class Classroon {
     list.add(6);
     list.add(8);
 
-    int max = Integer.MIN_VALUE;
-    for(int i =0;i<list.size();i++){
-      // if (max<list.get(i)) {
-      //   max = list.get(i);
-      // }
+    // System.out.println(list);
+    // Collections.sort(list); //ascending
+    // System.out.println(list);
 
-      max = Math.max(max, list.get(i)); 
-    }
-      System.out.println("max element = " + max);
-    }
+    // //descending
+    // Collections.sort(list,Collections.reverseOrder());
+    // System.out.println(list);
+
+    // // Swap Numbers
+    // int idx1 = 1, idx2 = 3;
+    // System.out.println(list);
+    // swap(list, idx1, idx2);
+    // System.out.println(list);
+
+    // Max numbers
+    // int max = Integer.MIN_VALUE;
+    // for(int i =0;i<list.size();i++){
+    // // if (max<list.get(i)) {
+    // // max = list.get(i);
+    // // }
+
+    // max = Math.max(max, list.get(i));
+    // }
+    // System.out.println("max element = " + max);
+    // }
 
     // list.add(1, 9);
     // System.out.println(list);
@@ -37,25 +59,27 @@ public class Classroon {
     // }
 
     // Reverse print
-  //   for (int i = list.size() - 1; i >= 0; i--) {
-  //     System.out.println(list.get(i) + " ");
-  //   }
-  //   System.out.println();
-  // }
+    // for (int i = list.size() - 1; i >= 0; i--) {
+    // System.out.println(list.get(i) + " ");
+    // }
+    // System.out.println();
+    // }
 
-  // Get Operation
-  // int element = list.get(2);
-  // System.out.println(element);
+    // Get Operation
+    // int element = list.get(2);
+    // System.out.println(element);
 
-  // //Remove operation
-  // list.remove(2);
-  // System.out.println(list);
+    // //Remove operation
+    // list.remove(2);
+    // System.out.println(list);
 
-  // //Set element at index
-  // list.set(2,10);
-  // System.out.println(list);
+    // //Set element at index
+    // list.set(2,10);
+    // System.out.println(list);
 
-  // //contains element
-  // System.out.println(list.contains(1));
-  // System.out.println(list.contains(11));
+    // //contains element
+    // System.out.println(list.contains(1));
+    // System.out.println(list.contains(11));
+  }
+
 }
