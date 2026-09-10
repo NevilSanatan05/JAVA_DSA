c5a2c1b2cbcb4811872a21d58d8f7fadbec611a2
nevil811 <nevilbarick69@gmail.com>
2026-09-10 13:29:55 +0530

    ArrayList

---
+++ Arrays List/Classroom2.java
@@ -0,0 +1,64 @@
+// import java.util.*;
+
+// public class Classroom2 {
+//   public static void main(String[] args) {
+
+//     ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+//     ArrayList<Integer> list = new ArrayList<>();
+//     list.add(1);
+//     list.add(2);
+//     mainList.add(list);
+
+//     ArrayList<Integer> list2 = new ArrayList<>();
+//     list2.add(3);
+//     list2.add(4);
+//     mainList.add(list2);
+
+//     for (int i = 0; i < mainList.size(); i++) {
+//       ArrayList<Integer> currList = mainList.get(i);
+//       for (int j = 0; j < currList.size(); j++) {
+//         System.out.print(currList.get(j) + " ");
+//       }
+//       System.out.println();
+//     }
+//     System.out.println(mainList);
+//   }
+// }
+
+//Container with Most Water (Brute Force)
+import java.util.*;
+
+public class Classroom2 {
+
+  public static int storeWater(ArrayList<Integer> height) {
+    int maxWater = 0;
+    // brute force
+    for (int i = 0; i < height.size(); i++) {
+      for (int j = i + 1; j < height.size(); j++) {
+        int ht = Math.min(height.get(i), height.get(j));
+        int width = j - 1;
+        int currWater = ht * width;
+        maxWater = Math.max(maxWater, currWater);
+      }
+
+    }
+    return maxWater;
+
+  }
+
+  public static void main(String[] args) {
+    ArrayList<Integer> height = new ArrayList<>();
+    // 1,8,6,2,5,4,8,3,7
+    height.add(1);
+    height.add(8);
+    height.add(6);
+    height.add(2);
+    height.add(5);
+    height.add(4);
+    height.add(8);
+    height.add(3);
+    height.add(7);
+
+    System.out.println(storeWater(height));
+  }
+}
