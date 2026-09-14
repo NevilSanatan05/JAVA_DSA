00ce65503d3bcf48b00f1af4fda819f2b5480537
nevil811 <nevilbarick69@gmail.com>
2026-09-14 14:23:56 +0530

    Pairsum-2

--- Arrays List/Classroom2.java
+++ Arrays List/Classroom2.java
@@ -65,30 +65,67 @@
 
 
 import java.util.ArrayList;
-public class Classroom2{
-  //Brute Force
-  public static boolean pairSum1(ArrayList<Integer> list,int target){
-    for(int i=0;i<list.size();i++){
-      for(int j=i+1;j<list.size();j++){
-        if(list.get(i)+list.get(j)==target){
-          return true;
-        }
-      }
-    }
-  return false;
-}
-  public static void main(String[] args) {
-  ArrayList<Integer> list = new ArrayList<>();
-  //1,2,3,4,5,6
-  list.add(1);
-  list.add(2);
-  list.add(3);
-  list.add(4);
-  list.add(5);
-  list.add(6);
-  int target = 5;
-  System.out.println(pairSum1(list,target));
-  
-}
 
+public class Classroom2 {
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean pairSum2(ArrayList<Integer> list, int target) {
+        int bp = -1;
+        int n = list.size();
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                bp = i;
+                break;
+            }
+        }
+
+        if (bp == -1) {
+            int lp = 0;
+            int rp = n - 1;
+            while (lp < rp) {
+                if (list.get(lp) + list.get(rp) == target) return true;
+                if (list.get(lp) + list.get(rp) < target) lp++;
+                else rp--;
+            }
+            return false;
+        }
+
+        int lp = bp + 1;
+        int rp = bp;
+
+        while (lp != rp) {
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+            if (list.get(lp) + list.get(rp) < target) {
+                lp = (lp + 1) % n;
+            } else {
+                rp = (n + rp - 1) % n;
+            }
+        }
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16;
+        System.out.println(pairSum2(list, target));
+    }
 }
