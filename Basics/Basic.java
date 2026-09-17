0452b331e4d4fb5af697269f88a661beae55c705
nevil811 <nevilbarick69@gmail.com>
2026-09-17 15:52:17 +0530

    Revision

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,107 +1,122 @@
-// public class Basic{
-// public static void main(String args[]){
-// System.out.println("*****");
-// System.out.println(" * ");
-// System.out.println(" * ");
-// System.out.println(" * ");
-// System.out.println("*****");
-// }
-// }
+//Day1
+// //Java is case - sensitive
+// import java.util.Scanner;
 
+// public class Basic {
+//   public static void main(String[] args) {
+//     // System.out.println("Hello");
+//     // System.out.println("My name is Nevil");
+//     // System.out.println("I am Learning Java");
+//     // String name = "Nevil";
+//     // int age = 20;
+//     // double height = 5.8;
+//     // boolean student = true;
 
-// (*/) > (+-)
+//     // System.out.println("Name: " + name);
+//     // System.out.println("Age: " + age);
+//     // System.out.println("Height: " + height);
+//     // System.out.println("Student: " + student);
+//     Scanner sc = new Scanner(System.in);
+//     // System.out.print("Enter your name : ");
+//     // String name = sc.nextLine();
+//     // System.out.print("Enter your age : ");
+//     // int age = sc.nextInt();
+//     // System.out.print("Enter your height : ");
+//     // double height = sc.nextDouble();
+//     // System.out.println("Name: " + name);
+//     // System.out.println("Age: " + age);
+//     // System.out.println("Height: " + height);
+//     // System.out.print("Enter first number: ");
+//     // int a = sc.nextInt();
+//     // System.out.print("Enter second number: ");
+//     // int b = sc.nextInt();
 
+//     // System.out.println("Sum:" + (a + b));
+//     // System.out.println("Difference:" + (a - b));
+//     // System.out.println("Multiplication:" + (a * b));
+//     // System.out.println("Division:" + (a /b));
+//     // System.out.println("Remainder:" + (a%b));
+//     System.out.print("Enter your name: ");
+//     String name = sc.next();
+//     System.out.print("Enter your age: ");
+//     int age = sc.nextInt();
+//     System.out.print("Enter yout birth year: ");
+//     int year = sc.nextInt();
+// System.out.println("Name: "+name);
+// System.out.println("Age in 2026: "+ (2026-year));
 
-// Conditional Statements
-// public class Basic{
-// public static void main(String args[]){
-// int marks = 45;
-// if(marks>90){
-// System.out.println("Excellent");
-// }
-// else if (marks>80 && marks<=90){
-// System.out.println("good");
-// }
-// else if (marks>70 && marks<=80){
-// System.out.println("fair");
-// }
-// else if (marks>60 && marks<=70){
-// System.out.println("meets expectations");
-// }
-// else{
-// System.out.println("below par");
-// }
-// }
-// }
-
-
-// Loops
-// While Loop
-// public class Basic{
-// public static void main(String args[]){
-// int i =0;
-// while(i<=10){
-// System.out.println(i);
-// i++;
-// }
-// }
-// }
-
-// For Loop
-// public class Basic{
-// public static void main(String args[]){
-// for(int i=1;i<=10;i++){
-// System.out.println(i);
-// }
-// }
-// }
-
-// Do While Loop
-// public class Basic{
-// public static void main(String args[]){
-// int i =1;
-// do{
-// System.out.println(i);
-// i++;
-// }
-// while(i<=10);
-// }
-// }
-
-
-// import java.util.*;
-// public class Basic{
-//   public static void main(String args[]){
-   
-//    Scanner sc = new Scanner(System.in);
-//    System.out.print("Enter your age : ");
-//    int i = sc.nextInt();
-//    if(i>=18){
-    
-//      System.out.println("Eligible to vote");
-//    }
-//    else{
-//      System.out.println("Not Eligible to vote");
-//    } 
-//    System.out.print("Enter your name : ");
-//    String name = sc.next();
-//    System.out.println("Hello " + name);
-
+//     sc.close();
 //   }
 // }
 
-
-//isPrime
-
 import java.util.Scanner;
-public class Basic {
 
+public class Basic {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a number : ");
-    int n = sc.nextInt();
-    System.out.println(n);
+    // System.out.print("Enter first number: ");
+    // int a = sc.nextInt();
+
+    // System.out.print("Enter second number: ");
+    // int b = sc.nextInt();
+    // System.out.println("Greater: "+ (a>b));
+    // System.out.println("Smaller: "+ (a<b));
+    // System.out.println("Equa;: "+ (a==b));
+    // System.out.println("Different: "+ (a!=b));
+
+    // System.out.print("Enter number: ");
+    // int a = sc.nextInt();
+    // if(a%2==0){
+    //   System.out.println("Even number");
+    // }
+    // else{
+    //   System.out.println("Odd number");
+    // }
+// if(a>b){
+//   System.out.println(a +" is larger");
+// }
+// else{
+//   System.out.println(b + "is larger");
+// }
+
+// System.out.print("Enter marks: ");
+// int marks = sc.nextInt();
+// if(marks>=90 && marks<=100){
+//   System.out.println("Excellent");
+// }
+// else if(marks>=75 && marks<90){
+//   System.out.println("Good");
+// }
+// else if(marks>=50 && marks<75){
+//   System.out.println("Average");
+// }
+// else{
+//   System.out.println("Fail");
+// }
+// System.out.print("Enter day: ");
+// int day = sc.nextInt();
+// if(day == 6 || day==7){
+//   System.out.println("Weekend");
+// }
+// else{
+//   System.out.println("Weekday");
+// }
+// System.out.print("Enter age: ");
+// int age = sc.nextInt();
+// if(age>=18 && age<=60){
+//   System.out.println("Eligible");
+// }
+// else{
+//   System.out.println("Not Eligible");
+// }
+System.out.print("Are you a student? ");
+boolean student = sc.nextBoolean();
+if(!student){
+  System.out.println("Not a student");
+}
+else{
+  System.out.println("Is a student");
+}
     sc.close();
   }
-
 }
