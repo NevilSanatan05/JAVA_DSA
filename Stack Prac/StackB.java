fdbb6de07e8845efbd323207328fc3b154bc72d3
nevil811 <nevilbarick69@gmail.com>
2026-09-17 14:58:00 +0530

    Stack using JAVA Collection Framework

--- Stack Prac/StackB.java
+++ Stack Prac/StackB.java
@@ -165,3 +165,26 @@
 // System.out.println(s.search(10));
 //   }
 // }
+
+
+// import java.util.Stack;
+// public class StackB{
+//   public static void pushAtBottom(Stack<Integer> s,int data){
+//     if(s.isEmpty()){
+//       s.push(data);
+//       return;
+//     }
+//     int top = s.pop();
+//     pushAtBottom(s, data);
+//     s.push(top);
+//   }
+//   public static void main(String[] args) {
+//     Stack<Integer> s = new Stack<>();
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
+//     System.out.println("Before" + s);
+//     pushAtBottom(s, 40);
+//     System.out.println("After" + s);
+//   }
+// }
