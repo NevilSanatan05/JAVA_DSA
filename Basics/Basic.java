5b88fdb91d81b441f074b63a642a844211df702d
nevil811 <nevilbarick69@gmail.com>
2026-09-23 16:21:51 +0530

    Revision

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,11 +1,8 @@
-public class Basic {
-
+public class Basic{
   public static void main(String[] args) {
-   int i =0;
-   do{
-    i++;
-    System.out.println(i);
-   }
-   while(i<5);
+int arr[] = {10, 20, 30, 40, 50};
+for(int i=arr.length-1;i>=0;i--){
+  System.out.println(arr[i]);
+}   
+    }
   }
-}
