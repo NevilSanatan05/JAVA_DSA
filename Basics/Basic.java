994a55f1bedb73ec9ea2499aaaa802e302db1a31
nevil811 <nevilbarick69@gmail.com>
2026-09-24 15:04:35 +0530

    Revision 1

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,8 +1,218 @@
-public class Basic{
-  public static void main(String[] args) {
-int arr[] = {10, 20, 30, 40, 50};
-for(int i=arr.length-1;i>=0;i--){
-  System.out.println(arr[i]);
-}   
-    }
-  }
+//Revision-1
+
+// import java.util.*;
+// public class Basic{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     if(n%2==0){
+//       System.out.println(n + " is even number");
+//     }
+// else{
+//   System.out.println(n + " is odd number");
+// }
+//   }
+// }
+
+//Revision - 2
+
+// import java.util.*;
+// public class Basic {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the age : ");
+//     int age = sc.nextInt();
+//     if (age >= 18 && age <= 59) {
+//       System.out.println("Eligible to vote");
+//     } else if (age < 18) {
+//       System.out.println("Below voting age");
+//     } else {
+//       System.out.println("Senior citizen");
+//     }
+//   }
+// }
+
+//Revision-3
+
+// import java.util.*;
+// public class Basic{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     if(n%2==0){
+//       System.out.println(n + " is even number");
+//     }
+// else{
+//   System.out.println(n + " is odd number");
+// }
+//   }
+// }
+
+//Revision - 4
+
+// import java.util.*;
+// public class Basic {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     for (int i = 1; i <= n; i++) {
+//       if (i % 2 == 0) {
+//         System.out.println(i);
+//       }
+//     }
+//   }
+// }
+
+//Revision - 5
+// import java.util.*;
+// public class Basic {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     int sum = 0;
+//     for (int i = 0; i <= n; i++) {
+//       sum = sum + i;
+//     }
+//     System.out.println(sum);
+//   }
+// }
+
+//Revision - 6
+// public class Basic {
+//   static int square(int n){
+//     return n*n;
+//   }
+//   public static void main(String[] args) {
+//    int result = square(5);
+//    System.out.println(result);
+//   }
+// }
+
+//Revision - 7
+// public class Basic {
+//   public static void main(String[] args) {
+//     int arr[] = { 10, 25, 7, 40, 15 };
+//     int max = arr[0];
+//     for (int i = 0; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//         max = arr[i];
+//       }
+//     }
+//     System.out.println(max);
+//   }
+// }
+
+
+//Revision - 8
+//Revision-1
+
+// import java.util.*;
+// public class Basic{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     if(n%2==0){
+//       System.out.println(n + " is even number");
+//     }
+// else{
+//   System.out.println(n + " is odd number");
+// }
+//   }
+// }
+
+//Revision - 2
+
+// import java.util.*;
+// public class Basic {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the age : ");
+//     int age = sc.nextInt();
+//     if (age >= 18 && age <= 59) {
+//       System.out.println("Eligible to vote");
+//     } else if (age < 18) {
+//       System.out.println("Below voting age");
+//     } else {
+//       System.out.println("Senior citizen");
+//     }
+//   }
+// }
+
+//Revision-3
+
+// import java.util.*;
+// public class Basic{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     if(n%2==0){
+//       System.out.println(n + " is even number");
+//     }
+// else{
+//   System.out.println(n + " is odd number");
+// }
+//   }
+// }
+
+//Revision - 4
+
+// import java.util.*;
+// public class Basic {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     for (int i = 1; i <= n; i++) {
+//       if (i % 2 == 0) {
+//         System.out.println(i);
+//       }
+//     }
+//   }
+// }
+
+//Revision - 5
+// import java.util.*;
+// public class Basic {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter an input n : ");
+//     int n = sc.nextInt();
+//     int sum = 0;
+//     for (int i = 0; i <= n; i++) {
+//       sum = sum + i;
+//     }
+//     System.out.println(sum);
+//   }
+// }
+
+//Revision - 6
+// public class Basic {
+//   static int square(int n){
+//     return n*n;
+//   }
+//   public static void main(String[] args) {
+//    int result = square(5);
+//    System.out.println(result);
+//   }
+// }
+
+//Revision - 7
+// public class Basic {
+//   public static void main(String[] args) {
+//   int arr[]={10,15,22,31,40,55,60};
+//   int count = 0;
+//   for(int i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//       count++;
+//     }
+//   }
+//   System.out.println(count);
+//   }
+// }
+
