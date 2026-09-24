75212c99ea2d27ca69bd72965ac054b674dba259
nevil811 <nevilbarick69@gmail.com>
2026-09-24 15:26:19 +0530

    Revision 2

---
+++ Basics/Basic2.java
@@ -0,0 +1,10 @@
+public class Basic2{
+  public static void main(String[] args) {
+    int arr[]={10,20,10,30,10,40};
+    int count = 0;
+    for(int i=0;i<arr.length;i++){
+      
+    }
+   
+    }
+  }
