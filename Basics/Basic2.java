9bdd90fc577d6d89a79c6cab8e9fad2a9bd37c33
nevil811 <nevilbarick69@gmail.com>
2026-09-25 15:39:22 +0530

    Revision 2

--- Basics/Basic2.java
+++ Basics/Basic2.java
@@ -1,10 +1,9 @@
 public class Basic2{
   public static void main(String[] args) {
-    int arr[]={10,20,10,30,10,40};
-    int count = 0;
-    for(int i=0;i<arr.length;i++){
-      
-    }
-   
-    }
+    int arr[]={10,20,30,40,50};
+    int index = 2;
+    int newArr[]=new int[arr.length-1];
+    for()
+
   }
+}
